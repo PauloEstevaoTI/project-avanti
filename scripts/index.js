67167fd69c1d812678052d7aa9ec6b1f5f890c28
Bombeta <paullo_jf@hotmail.com>
2025-04-18 19:09:20 -0300

    feature: create header and main content

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById(id).innerHTML = html;
+    });
+}
+
+//loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
